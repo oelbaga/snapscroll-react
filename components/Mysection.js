89d94ad01052a3a14a65c358fc7075868a8ca5eb
Omar Elbaga <oelbaga@newworldgroup.com>
2022-08-19 23:24:28 -0400

    first commit

---
+++ components/Mysection.js
@@ -0,0 +1,54 @@
+import { useEffect, useRef } from "react";
+import Image from "next/image";
+import styles from "./Mysection.module.scss";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+export default function Mysection({
+  image,
+  headline,
+  scrollTo,
+  goToSectionRef,
+  showArrow,
+}) {
+  const headlineRef = useRef();
+  const sectionRef = useRef();
+  useEffect(() => {
+    gsap.fromTo(
+      headlineRef.current,
+      {
+        opacity: 0,
+        y: -20,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power3.out",
+        scrollTrigger: {
+          scroller: ".container",
+          trigger: headlineRef.current,
+          start: "top 80%",
+          end: "bottom 0%",
+          toggleActions: "play none none reverse",
+        },
+      }
+    );
+    return () => {};
+  }, []);
+  return (
+    <div className={styles.section} ref={sectionRef}>
+      <div className={styles.copy}>
+        <h2 ref={headlineRef}>{headline}</h2>
+      </div>
+      <Image src={image} layout={`fill`} />
+      {showArrow && (
+        <button
+          className={styles.downarrow}
+          onClick={() => scrollTo(goToSectionRef)}
+        ></button>
+      )}
+    </div>
+  );
+}
